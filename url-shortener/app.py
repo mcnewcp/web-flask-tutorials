1e10d3716148df63470721e95aff54b631413da2
Coy McNew <mcnewcp@gmail.com>
2022-09-07 11:30:59 -0500

    initialize json for url data storage

--- url-shortener/app.py
+++ url-shortener/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask.helpers import url_for
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +12,12 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        #make a dictionary with key=short url, val=full url
+        urls = {}
+        urls[request.form['code']] = {'url':request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+        
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
