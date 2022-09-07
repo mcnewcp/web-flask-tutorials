73cbe36b53559c7ceffe198b0a661a0cde3724ba
Coy McNew <mcnewcp@gmail.com>
2022-09-07 11:37:03 -0500

    allow for short url checking and saving

--- url-shortener/app.py
+++ url-shortener/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask.helpers import url_for
 import json
+import os.path
 
 app = Flask(__name__)
 
@@ -12,9 +13,22 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
-        #make a dictionary with key=short url, val=full url
+        #data stored in dictionary with key=short url, val=full url
         urls = {}
+
+        #open json if it exists
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+        
+        #check if short url has been used
+        if request.form['code'] in urls.keys():
+            return redirect(url_for('home'))
+
+        #write new data
         urls[request.form['code']] = {'url':request.form['url']}
+        
+        #write to json
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         
